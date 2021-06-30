c07faaf7bd93c1e344b14653e0f9a0e422fd88f7
kingken40 <45611070+kingken40@users.noreply.github.com>
2021-06-30 10:38:09 -0600

    Update uimain.c

--- src/uimain.c
+++ src/uimain.c
@@ -1,57 +1,4 @@
 #include <stdio.h>
-<<<<<<< HEAD
-
-#include <stdlib.h>
-
-#include "tokenizer.h"
-
-#include "history.h"
-
-
-
-int main(){
-
-  printf("\n$ ");
-
-  char user_input[100];
-
-  fgets(user_input, 100, stdin);
-
-
-
-  // testing function space char / non space char (WORKS)
-
-  //int j = space_char(*user_input);
-
-  //printf("%d\n",j);
-
-
-
-  //testing function word start (WORKS)
-
-  //char* start = word_start(user_input);
-
-  //printf("\n%s", "Word start running: \n");
-
-  //printf("%c\n", *start);
-
-
-
-  //testing function word terminator (Works)
-
-  //char* end = word_terminator(start);
-
-  //printf("%c\n", *end);
-
-
-
-  //testing count words function
-
-  printf("\n%s", "Count words Running: \n");
-
-  printf("%d\n", count_words(user_input));
-
-=======
 #include <stdlib.h>
 #include "tokenizer.h"
 #include "history.h"
@@ -90,8 +37,4 @@ int main(){
   printf("%s\n", new_string);
 
 
-
-
-  
->>>>>>> 84b9044dd65e8ae837d0a6e05fdbd4e4dfd9526b
 }
